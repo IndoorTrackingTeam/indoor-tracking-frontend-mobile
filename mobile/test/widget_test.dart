6a0ede516dab4ce74a826766da05e103a27276e4
PedroPGuimaraes <pedro.pereira@gec.inatel.br>
2024-07-12 17:39:22 -0300

    test: added login screen test

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,29 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile/main.dart';
+import 'package:mobile/screens/LoginScreen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  group('LoginScreen Widget Tests', () {
+    testWidgets('Login screen has email and password fields and a login button',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: LoginScreen()));
+      expect(find.text('Olá novamente!'), findsOneWidget);
+      expect(find.byType(TextFormField), findsNWidgets(2));
+      expect(find.text('LOGIN'), findsOneWidget);
+    });
   });
 }
