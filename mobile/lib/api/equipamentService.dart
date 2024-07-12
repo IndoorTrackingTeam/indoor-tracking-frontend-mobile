493d5f3502a14e4dc503b8e09cd749de30f1ae13
PedroPGuimaraes <pedro.pereira@gec.inatel.br>
2024-07-12 17:38:16 -0300

    feat: added api services

---
+++ mobile/lib/api/equipamentService.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String path = 'https://run-api-viicaovoda-ue.a.run.app';
+
+class EquipamentService {
+  Future<List<dynamic>> getEquipaments() async {
+    var url = Uri.parse("$path/equipment/read-all");
+    var response = await http.get(url);
+
+    try {
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception('Failed to load Equipaments');
+      }
+    } catch (e) {
+      return Future.error('Failed to $e');
+    }
+  }
+}
--- mobile/lib/api/userService.dart
+++ mobile/lib/api/userService.dart
@@ -1,71 +1,73 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 const String path = 'https://run-api-viicaovoda-ue.a.run.app';
 
-Future<String> signInEmailPassword(String email, String password) async {
-  final url = Uri.parse('$path/user/login');
+class UserService {
+  Future<String> signInEmailPassword(String email, String password) async {
+    final url = Uri.parse('$path/user/login');
 
-  final headers = {
-    'Content-Type': 'application/json',
-  };
+    final headers = {
+      'Content-Type': 'application/json',
+    };
 
-  final body = jsonEncode({
-    "email": email,
-    "password": password,
-  });
+    final body = jsonEncode({
+      "email": email,
+      "password": password,
+    });
 
-  try {
-    final response = await http.post(
-      url,
-      headers: headers,
-      body: body,
-    );
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      final oid = responseData['_id']['\$oid'];
-      return oid;
-    } else {
-      final errorMessage =
-          jsonDecode(response.body)['message'] ?? 'Unknown error';
-      throw Exception('Failed to authenticate: $errorMessage');
+    try {
+      final response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        final oid = responseData['_id']['\$oid'];
+        return oid;
+      } else {
+        final errorMessage =
+            jsonDecode(response.body)['message'] ?? 'Unknown error';
+        throw Exception('Failed to authenticate: $errorMessage');
+      }
+    } catch (e) {
+      throw Exception(
+          'An error occurred while authenticating: ${e.toString()}');
     }
-  } catch (e) {
-    throw Exception('An error occurred while authenticating: ${e.toString()}');
   }
-}
 
-Future<String> signUpEmailPassword(
-    String name, String register, String password, String email) async {
-  final url = Uri.parse('$path/user/create');
+  Future<String> signUpEmailPassword(
+      String name, String register, String password, String email) async {
+    final url = Uri.parse('$path/user/create');
 
-  final headers = {
-    'Content-Type': 'application/json',
-  };
+    final headers = {
+      'Content-Type': 'application/json',
+    };
 
-  final body = jsonEncode({
-    "name": name,
-    "email": email,
-    "password": password,
-    "register_": register,
-  });
+    final body = jsonEncode({
+      "name": name,
+      "email": email,
+      "password": password,
+      "register_": register,
+    });
 
-  try {
-    final response = await http.post(
-      url,
-      headers: headers,
-      body: body,
-    );
-    if (response.statusCode == 201) {
-      return 'Success';
-    } else {
-      final errorMessage =
-          jsonDecode(response.body)['message'] ?? 'Unknown error';
-      throw Exception('Failed to register user: $errorMessage');
+    try {
+      final response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+      if (response.statusCode == 201) {
+        return 'Success';
+      } else {
+        final errorMessage =
+            jsonDecode(response.body)['message'] ?? 'Unknown error';
+        throw Exception('Failed to register user: $errorMessage');
+      }
+    } catch (e) {
+      throw Exception(
+          'An error occurred while registering user: ${e.toString()}');
     }
-  } catch (e) {
-    throw Exception(
-        'An error occurred while registering user: ${e.toString()}');
   }
 }
